0193baa4a6ff69851f50a85b219126c4ad1a9dee
Moscicki <dimulgator@outlook.com>
2025-05-06 21:33:47 +0300

    chore: change lib.rs

--- k8s-rs-pb/src/lib.rs
+++ k8s-rs-pb/src/lib.rs
@@ -1,9 +1,16 @@
 #![allow(non_snake_case)]
-#[cfg(feature = "v1_30")] pub mod v1_30;
-#[cfg(feature = "v1_30")] pub use self::v1_30::*;
-#[cfg(feature = "v1_31")] pub mod v1_31;
-#[cfg(feature = "v1_31")] pub use self::v1_31::*;
-#[cfg(feature = "v1_32")] pub mod v1_32;
-#[cfg(feature = "v1_32")] pub use self::v1_32::*;
-#[cfg(feature = "v1_33")] pub mod v1_33;
-#[cfg(feature = "v1_33")] pub use self::v1_33::*;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "v1_30")] {
+        pub mod v1_30;
+        pub use self::v1_30::*;
+    } else if #[cfg(feature = "v1_31")] {
+        pub mod v1_31;
+        pub use self::v1_31::*;
+    } else if #[cfg(feature = "v1_32")] {
+        pub mod v1_32;
+        pub use self::v1_32::*;
+    } else if #[cfg(feature = "v1_33")] {
+        pub mod v1_33;
+        pub use self::v1_33::*;
+    }
+}
